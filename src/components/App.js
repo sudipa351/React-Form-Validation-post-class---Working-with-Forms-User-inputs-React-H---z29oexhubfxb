418e9e1070759ce4c7410c3fd77c71148e676dda
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-12-27 03:52:19 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,66 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 /**
  * @task :add validation to email, if email is not valid, if not valid email, dont allow to submit
- * @error_message :  "Email is invalid"  if email is wrong. (must be same message) 
- * 
- * 
+ * @error_message :  "Email is invalid"  if email is wrong. (must be same message)
+ *
+ *
  */
 
 function App() {
+  /**
+   * code here
+   */
+  const fnameRef = useRef();
+  const emailRef = useRef();
+  const [error, setError] = useState(undefined);
+  const [data, setData] = useState({ fname: undefined, lname: undefined });
 
- /**
-  * code here
-  */
+  const change = () => {
+    // console.log(emailRef.current.value);
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(emailRef.current.value)) {
+      setError(undefined);
+      document.getElementById("submit").disabled = false;
+    } else {
+      setError("Email is invalid");
+      document.getElementById("submit").disabled = true;
+    }
+  };
 
-  return(
+  return (
     <div className="App">
       <h1>How About Them Apples</h1>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setData({
+            fname: fnameRef.current.value,
+            lname: emailRef.current.value,
+          });
+        }}
+      >
         <fieldset>
           <label>
             <p>First Name</p>
-            <input id='fname' name="name"  ref={fnameRef}/>
+            <input id="fname" name="name" ref={fnameRef} />
             <br></br>
             <p>Email</p>
-            <input id='lname' name="name"   ref={emailRef}/>
-            {error && <h2 style={{color: 'red'}}>{error}</h2>}
+            <input id="lname" name="name" onChange={change} ref={emailRef} />
+            {error && <h2 style={{ color: "red" }}>{error}</h2>}
           </label>
         </fieldset>
 
-        <button id='submit' type="submit">Submit</button>
+        <button id="submit" type="submit">
+          Submit
+        </button>
       </form>
-      {
-        data.fname != undefined && (
-          <div>
+      {data.fname != undefined && (
+        <div>
           <h1>{data.fname}</h1>
           <h2>{data.lname}</h2>
-          </div>
-        )
-      }
+        </div>
+      )}
     </div>
-  )
+  );
 }
-
 
 export default App;
